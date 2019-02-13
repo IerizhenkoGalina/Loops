bc4266607d08665b56d399ffd8ddbf217f055887
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-13 15:53:19 +0200

    Create for.js

---
+++ for.js
@@ -0,0 +1,22 @@
+for (let number = 5; number < 11; number++) {
+  console.log(number);
+};
+//Prints:
+/*
+5
+6
+7
+8
+9
+10
+*/
+for (let counter = 3; counter >= 0; counter--){
+  console.log(counter)
+};
+//Prints:
+/*
+3
+2
+1
+0
+*/
