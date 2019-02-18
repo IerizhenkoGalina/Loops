a7fcc42c0d268a6caf9a766e36e5bb31cf79188b
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-18 08:54:46 +0200

    Create do...while.js

---
+++ do...while.js
@@ -0,0 +1,18 @@
+let cityString='';
+let i=0;
+do {
+cityString=cityString +i;
+i++;
+} while (i<5);
+console.log(cityString);
+//Prints: 01234
+
+const firstMessage="I will print!";
+const secondMessage="I will not print!";
+do{
+connsole.log(firstMessage)
+} while(true===false);
+while(true===false){
+console.log(secondMessage)
+};
+//Prints: I will print!
